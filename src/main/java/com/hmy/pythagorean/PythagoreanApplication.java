a8e90d0b2f6e26e62af78e77c6ffe67cc57c047a
mingyuanhua <m.y.hua@hotmail.com>
2023-05-10 20:34:28 -0700

    initial commit

---
+++ src/main/java/com/hmy/pythagorean/PythagoreanApplication.java
@@ -0,0 +1,13 @@
+package com.hmy.pythagorean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PythagoreanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PythagoreanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hmy/pythagorean/PythagoreanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmy.pythagorean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PythagoreanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
