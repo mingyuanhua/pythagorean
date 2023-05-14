da227373b10095aaf3afff5f6c155e6a0d776538
mingyuanhua <m.y.hua@hotmail.com>
2023-05-14 15:27:07 -0700

    Add fake data for hello controller

--- src/main/java/com/hmy/pythagorean/hello/HelloWorldController.java
+++ src/main/java/com/hmy/pythagorean/hello/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.hmy.pythagorean.hello;
 
+import com.github.javafaker.Faker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello World!";
+        Faker faker = new Faker();
+        String name = faker.name().fullName();
+        String company = faker.company().name();
+        String street = faker.address().streetAddress();
+        String city = faker.address().city();
+        String state = faker.address().state();
+        String bookTitle = faker.book().title();
+        String bookAuthor = faker.book().author();
+
+        String template = "This is %s\n" +
+                "I work at %s\n" +
+                "I live at %s in %s %s\n" +
+                "My favorite book is %s by %s\n";
+
+        return template.formatted(
+                name,
+                company,
+                street,
+                city,
+                state,
+                bookTitle,
+                bookAuthor
+        );
     }
 }
