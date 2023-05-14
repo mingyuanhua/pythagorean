5d2e4b09dec29457d6f4a0468a5566f3935298ca
mingyuanhua <m.y.hua@hotmail.com>
2023-05-14 15:56:19 -0700

    Add query parameter locale

--- src/main/java/com/hmy/pythagorean/hello/HelloWorldController.java
+++ src/main/java/com/hmy/pythagorean/hello/HelloWorldController.java
@@ -2,13 +2,19 @@ package com.hmy.pythagorean.hello;
 
 import com.github.javafaker.Faker;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
     @GetMapping("/hello")
-    public Person sayHello() {
-        Faker faker = new Faker();
+    public Person sayHello(@RequestParam(required = false) String locale) {
+        if (locale == null) {
+            locale = "en_US";
+        }
+        Faker faker = new Faker(new Locale(locale));
         String name = faker.name().fullName();
         String company = faker.company().name();
         String street = faker.address().streetAddress();
