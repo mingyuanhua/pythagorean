cb232b2d359ec61d57324762546d034d693033e3
mingyuanhua <m.y.hua@hotmail.com>
2023-05-19 16:06:18 -0700

    Add 5 entities for DB table mapping

---
+++ src/main/java/com/hmy/pythagorean/entity/MenuItemEntity.java
@@ -0,0 +1,15 @@
+package com.hmy.pythagorean.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("menu_items")
+public record MenuItemEntity(
+        @Id Long id,
+        Long restaurantId,
+        String name,
+        String description,
+        String price,
+        String imageUrl
+) {
+}
