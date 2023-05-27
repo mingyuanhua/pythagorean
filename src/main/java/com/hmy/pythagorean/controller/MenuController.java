96684e58034c29f7b20f50b2eb501ba54de69220
mingyuanhua <m.y.hua@hotmail.com>
2023-05-27 15:09:56 -0700

    Add MenuController and CartController

---
+++ src/main/java/com/hmy/pythagorean/controller/MenuController.java
@@ -0,0 +1,32 @@
+package com.hmy.pythagorean.controller;
+
+import com.hmy.pythagorean.entity.MenuItemEntity;
+import com.hmy.pythagorean.model.RestaurantDto;
+import com.hmy.pythagorean.service.MenuItemService;
+import com.hmy.pythagorean.service.RestaurantService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MenuController {
+    private final RestaurantService restaurantService;
+    private final MenuItemService menuItemService;
+
+    public MenuController(RestaurantService restaurantService, MenuItemService menuItemService) {
+        this.restaurantService = restaurantService;
+        this.menuItemService = menuItemService;
+    }
+
+    @GetMapping("/restaurant/{restaurantId}/menu")
+    public List<MenuItemEntity> getMenuByRestaurant(@PathVariable("restaurantId") long restaurantId) {
+        return menuItemService.getMenuItemsByRestaurantId(restaurantId);
+    }
+
+    @GetMapping("/restaurants/menu")
+    public List<RestaurantDto> getMenuForAllRestaurants() {
+        return restaurantService.getRestaurants();
+    }
+}
