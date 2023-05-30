b2fac9bd19ef03632199ca1a1f62f4869d3ae838
mingyuanhua <m.y.hua@hotmail.com>
2023-05-29 21:01:29 -0700

    Create CustomerController

--- src/main/java/com/hmy/pythagorean/AppConfig.java
+++ src/main/java/com/hmy/pythagorean/AppConfig.java
@@ -1,16 +1,25 @@
 package com.hmy.pythagorean;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 import javax.sql.DataSource;
-
 @Configuration
 public class AppConfig {
+
+
     @Bean
     UserDetailsManager users(DataSource dataSource) {
         JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
@@ -26,4 +35,31 @@ public class AppConfig {
     PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                .requestMatchers(HttpMethod.GET, "/", "/index.html", "/*.json", "/*.png", "/static/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/login", "/logout", "/signup").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/restaurants/**", "/restaurant/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and()
+                .formLogin()
+                .successHandler((req, res, auth) -> res.setStatus(HttpStatus.OK.value()))
+                .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+                .and()
+                .logout()
+                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK));
+        return http.build();
+    }
+
+
 }
--- src/main/java/com/hmy/pythagorean/controller/CustomerController.java
+++ src/main/java/com/hmy/pythagorean/controller/CustomerController.java
@@ -2,11 +2,24 @@ package com.hmy.pythagorean.controller;
 
 import com.hmy.pythagorean.model.RegisterBody;
 import com.hmy.pythagorean.service.CustomerService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
 
 @RestController
 public class CustomerController {
@@ -26,4 +39,5 @@ public class CustomerController {
         customerService.signUp(body.email(), body.password(), body.firstName(), body.lastName());
     }
 }
+
 
