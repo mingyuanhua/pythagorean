fce0fcc2c979658abc420c8bf3948813ef00d5c4
mingyuanhua <m.y.hua@hotmail.com>
2023-05-29 20:29:04 -0700

    Create CustomerController and Implement SignUp

---
+++ src/main/java/com/hmy/pythagorean/controller/CustomerController.java
@@ -0,0 +1,29 @@
+package com.hmy.pythagorean.controller;
+
+import com.hmy.pythagorean.model.RegisterBody;
+import com.hmy.pythagorean.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+
+    private final CustomerService customerService;
+
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+
+    @PostMapping("/signup")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void signUp(@RequestBody RegisterBody body) {
+        customerService.signUp(body.email(), body.password(), body.firstName(), body.lastName());
+    }
+}
+
