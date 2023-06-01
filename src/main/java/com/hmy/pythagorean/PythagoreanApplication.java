8ab87a39114fb724752fedec909ab4c31b0ded41
mingyuanhua <m.y.hua@hotmail.com>
2023-05-31 20:16:20 -0700

    Enable caching

--- src/main/java/com/hmy/pythagorean/PythagoreanApplication.java
+++ src/main/java/com/hmy/pythagorean/PythagoreanApplication.java
@@ -2,8 +2,10 @@ package com.hmy.pythagorean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PythagoreanApplication {
 
     public static void main(String[] args) {
